4b56f514119d9d2c4e560ad82d57d3052fb152c4
VencaJagellu <145977272+VencaJagellu@users.noreply.github.com>
2024-05-06 11:25:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Antonín Šimánek, Petr Janoušek, David Veselý">
+    <meta name="keywords" content="kontakty, email, odlov">
+    <title>Hlavní stránka</title>
+    <link rel="stylesheet" href="vzhled.css">
+</head>
+<!-- BARVY https://coolors.co/04151f-183a37-efd6ac-c44900-432534
+Plešaté hlavy, náš chléb - motto firmy
+Dobromilovci s.r.o.
+-->
+<body>
+    <header>
+        <h1>Dobromilovci</h1>
 
+        <nav>
+            <a href="index.php">Domů</a>
+            <a href="form.php">Produkty a služby</a>
+            <a href="onas.php">O nás</a>
+            <a href="kontakty.php">Kontakty</a>
+        </nav>
+
+    </header>
+
+    <main>
+
+    </main>
+</body>
+
+<footer>
+Copyright &copy; 2024 Dobromilovci a.s. All Rights reserved
+</footer>
+</html>
