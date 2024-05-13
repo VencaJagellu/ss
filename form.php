7d54dbdb6a6d6a3968a8b46977815c97b16ac3b3
VencaJagellu <145977272+VencaJagellu@users.noreply.github.com>
2024-05-13 10:07:38 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,1 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+        <h1>Dobromilovci</h1>
 
+        <nav>
+            <a href="index.php">Domů</a>
+            <a href="form.php">Produkty a služby</a>
+            <a href="onas.php">O nás</a>
+            <a href="kontakty.php">Kontakty</a>
+        </nav>
+
+    </header>
+<fieldset>
+    <legend><h1>Objednejte si jednu z našich služeb!</h1></legend>
+  <label for="skull">Odchyt Skulletů: </label><input type="number" step="0" max="" value="0" name="skull" id="skull" class="sluzby">
+  <br>
+  <br>
+  <label for="mull">Odchyt Mulletů: </label><input type="number" step="0" max="" value="0" name="mull" id="mull" class="sluzby">
+    <br>
+    <br>
+    <label for="kom">Odchyt Komunistů: </label><input type="number" step="0" max="" value="0" name="kom" id="kom" class="sluzby">
+    <br>
+    <br>
+    <label for="furr">Odchyt Furrýků: </label><input type="number" step="0" max="" value="0" name="furr" id="furr" class="sluzby">
+    <br>
+    <br>
+    <label for="jeh">Odchyt Jehovistů: </label><input type="number" step="0" max="" value="0" name="jeh" id="jeh" class="sluzby">
+    <br>
+    <br>
+    <label for="kres">Odchyt Křesťanů: </label><input type="number" step="0" max="" value="0" name="kres" id="kres" class="sluzby">
+    <br>
+    <br>
+    <label for="ehm">Odchyt lidí z teplejších krajin: </label><input type="number" step="0" max="" value="0" name="ehm" id="ehm" class="sluzby">
+    <br>
+    <br>
+    <label for="slech">Odchyt šlechty: </label><input type="number" step="0" max="" value="0" name="slech" id="slech" class="sluzby">
+    <br>
+    <br>
+    <label for="delnik">Odchyt levných dělníků z východu: </label><input type="number" step="0" max="" value="0" name="delnik" id="delnik" class="sluzby">
+    <br>
+    <br>
+    <label for="delnik">Odchyt kebabových lidí: </label><input type="number" step="0" max="" value="0" name="delnik" id="delnik" class="sluzby">
+    <br>
+    <br>
+    <input type="submit" value="Dokonči objednávku" id="submit">
+    <br>
+    <br>
+</fieldset>
+<br>
+<footer>
+Copyright &copy; 2024 Dobromilovci a.s. All Rights reserved
+</footer>
+<script src="script.js"></script>
+</body>
+</html>
