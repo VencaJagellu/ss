3c513939b167ad265200fbac993f3f934f5cd02d
VencaJagellu <145977272+VencaJagellu@users.noreply.github.com>
2024-05-13 11:06:49 +0200

    Update onas.php

--- onas.php
+++ onas.php
@@ -21,13 +21,15 @@
     </header>
     <main>
         <h1>O nás</h1>
-        <p>V srdci industriálního města Pilsen se zrodila neobvyklá firma s názvem Dobromilovci. Založena v roce 1982 trojicí kamarádů s vášní pro extravagantní účesy, stala se tato firma průkopníkem v zachycování jedinečné krásy mulletu.
-
-Zakladatelé, Petr, Antonín, Otakar II. a David, se od počátku zaměřovali na portréty a pouliční fotografii, zachycujíce skullety ve všech jejich tvarech, barvách a kontextech. Od roku 2000 se zaměřujeme na odchyt i dalších různých typů lidí.
-
-Firma se ale neomezuje pouze na fotografování. Dobromilovci pořádají také skullet festivaly, kde se scházejí fanoušci mulletu z celého světa, aby oslavili svůj oblíbený účes a užili si živou hudbu, soutěže a další zábavu.
-
-Dnes je Dobromilovci legendou v oblasti fotografování a symbolem v odchytu skulletů apod. Jejich práce inspirovala generace fotografů a pomohla změnit vnímání skulletu z pouhého módního trendu na zábavnou hru lovu na skulleta.</p>
+        <p>V srdci industriálního města Pilsen se zrodila neobvyklá firma s názvem Dobromilovci. Založena v roce 1982 trojicí kamarádů s vášní pro extravagantní účesy, stala se tato firma průkopníkem v zachycování jedinečné krásy mulletu. Zakladatelé, Petr, Antonín, Otakar II. a David, se od počátku zaměřovali na portréty a pouliční fotografii, zachycujíce skullety ve všech jejich tvarech, barvách a kontextech. Od roku 2000 se zaměřujeme na odchyt i dalších různých typů lidí. Firma se ale neomezuje pouze na fotografování. Dobromilovci pořádají také skullet festivaly, kde se scházejí fanoušci mulletu z celého světa, aby oslavili svůj oblíbený účes a užili si živou hudbu, soutěže a další zábavu.Dnes je Dobromilovci legendou v oblasti fotografování a symbolem v odchytu skulletů apod. Jejich práce inspirovala generace fotografů a pomohla změnit vnímání skulletu z pouhého módního trendu na zábavnou hru lovu na skulleta.</p>
+        <h2>Naše motto</h2>
+        <p>
+            Našim revírem je dálnice.<br>
+            Naše tempo je vražedné.<br>
+            Naše služby jsou fotografování skulletů, jehovů a šlechty.<br>
+            Muži z Dobromilovců pracují ve dne, v noci.<br>
+            Našim úkolem je fotit naše občany.
+        </p>
     </main>
     
     
