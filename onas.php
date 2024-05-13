714f27b29f908d44bc7901aa6c16eeb1fa1de869
VencaJagellu <145977272+VencaJagellu@users.noreply.github.com>
2024-05-13 10:59:57 +0200

    Create onas.php

---
+++ onas.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Antonín Šimánek, Petr Janoušek, David Veselý">
+    <meta name="keywords" content="kontakty, email, odlov">
+    <title>Kontakty</title>
+    <link rel="stylesheet" href="vzhled.css">
+</head>
+<body>
+    <header>
+            <h1>Dobromilovci</h1>
+    
+            <nav>
+                <a href="index.php">Domů</a>
+                <a href="form.php">Produkty a služby</a>
+                <a href="onas.php">O nás</a>
+                <a href="kontakty.php">Kontakty</a>
+            </nav>
+    </header>
+    <main>
+        <h1>O nás</h1>
+        <p>V srdci industriálního města Pilsen se zrodila neobvyklá firma s názvem Dobromilovci. Založena v roce 1982 trojicí kamarádů s vášní pro extravagantní účesy, stala se tato firma průkopníkem v zachycování jedinečné krásy mulletu.
+
+Zakladatelé, Petr, Antonín, Otakar II. a David, se od počátku zaměřovali na portréty a pouliční fotografii, zachycujíce skullety ve všech jejich tvarech, barvách a kontextech. Od roku 2000 se zaměřujeme na odchyt i dalších různých typů lidí.
+
+Firma se ale neomezuje pouze na fotografování. Dobromilovci pořádají také skullet festivaly, kde se scházejí fanoušci mulletu z celého světa, aby oslavili svůj oblíbený účes a užili si živou hudbu, soutěže a další zábavu.
+
+Dnes je Dobromilovci legendou v oblasti fotografování a symbolem v odchytu skulletů apod. Jejich práce inspirovala generace fotografů a pomohla změnit vnímání skulletu z pouhého módního trendu na zábavnou hru lovu na skulleta.</p>
+    </main>
+    
+    
+    
+    </body>
+    <footer>
+    Copyright &copy; 2024 Dobromilovci s.r.o. All Rights reserved
+    </footer>
+    </html>
+    
